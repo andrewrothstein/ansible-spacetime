c7ad0017552941dfb81b6b397a913673d80ebb18
Andrew Rothstein <andrew.rothstein@gmail.com>
2023-09-03 22:39:41 -0400

    spacetime:0.6.1-beta

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.spacetime
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-spacetime/actions/workflows/build.yml/badge.svg)
+
+Installs the spacetimedb [cli](https://spacetimedb.com/).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.spacetime
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksum.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/clockworklabs/SpacetimeDB/releases/download
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local archive_type=${4:-tar.gz}
+    local platform="${os}-${arch}"
+    local url="${MIRROR}/v${ver}${file}/spacetime.${platform}.${archive_type}"
+    local lfile="${DIR}/spacetime.${ver}.${platform}.${archive_type}"
+
+    if [ ! -e $lfile ];
+    then
+        curl -sSLf -o $lfile $url
+    fi
+
+    # https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.6.1-beta/spacetime.darwin-amd64.tar.gz
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+}
+
+dlver () {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver darwin amd64
+    dl $ver darwin arm64
+    dl $ver linux amd64
+    dl $ver linux arm64
+}
+
+dlver ${1:-"0.6.1-beta"}
