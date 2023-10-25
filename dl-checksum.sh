fb07b178cda5dcb0732d6077cfbe39afd508090e
Andrew Rothstein <andrew.rothstein@gmail.com>
2023-10-25 20:28:16 +0000

    spacetime:0.7.1-beta-hotfix1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -17,7 +17,7 @@ dl()
     then
         curl -sSLf -o $lfile $url
     fi
-
+    # https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.7.1-beta-hotfix1/spacetime.linux-amd64.tar.gz
     # https://github.com/clockworklabs/SpacetimeDB/releases/download/v0.6.1-beta/spacetime.darwin-amd64.tar.gz
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
@@ -32,4 +32,4 @@ dlver () {
     dl $ver linux arm64
 }
 
-dlver ${1:-"0.6.1-beta"}
+dlver ${1:-"0.7.1-beta-hotfix1"}
